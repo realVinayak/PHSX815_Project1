1623779cf100dce7577dea297d41506c60c8e53b
realVinayak <realvinayak123@gmail.com>
2023-02-14 00:33:30 -0600

    Add analyzing minimum number of measurements

---
+++ analyze_min_n_fnr.py
@@ -0,0 +1,19 @@
+from file_list_utils import read_multi_list
+import matplotlib.pyplot as plt
+
+
+# Plots relationship between minimum number of measurements and standard
+# deviation to get false negative rate below threshold
+def main():
+    n_std_variation = read_multi_list('./outputs/n_sigma_variation.txt')
+    std_list = [x[1] for x in n_std_variation]
+    n_list = [x[0] for x in n_std_variation]
+    plt.plot(std_list, n_list, '--bo')
+    plt.xlabel('Standard Deviation \u03C3')
+    plt.ylabel('Number of Measurements')
+    plt.title('\u03B2 = 10\u207B\u00B3')
+    plt.savefig('./outputs/n_sigma_variation.png')
+
+
+if __name__ == '__main__':
+    main()
---
+++ get_min_n_fnr.py
@@ -0,0 +1,34 @@
+from file_list_utils import write_multi_list
+from analyze_data import get_analyzed_data
+from generate_data import MEAN_1, MEAN_2, N_experiments, N_measurements, SIGMA_RANGE, \
+    ALPHA, BETA
+
+
+# Gets minimum number of measurements for a fixed sigma needed to get false
+# negative rate below fnr_threshold. Increments by 3 each iteration to speed
+# up process
+def get_min_n_from_fnr(mean_1, mean_2, sigma, n_experiments, alpha, fnr_threshold):
+    n_measurements = 1
+    while get_analyzed_data(mean_1, mean_2, sigma, n_measurements, n_experiments,
+                            alpha,
+                            read_from_file=False)[-1] > fnr_threshold:
+        n_measurements += 3
+    return n_measurements
+
+
+# Gets minimum number of measurements for each sigma in sigma_range (applies
+# get_min_n_from_fnr) to each sigma in sigma_range
+def get_min_n_variation(mean_1, mean_2, n_experiments, alpha, fnr_threshold,
+                        sigma_range):
+    min_n_variation = []
+    for sigma in sigma_range:
+        min_n = get_min_n_from_fnr(mean_1, mean_2, sigma, n_experiments, alpha,
+                                   fnr_threshold)
+        print('min n = ', min_n, 'for sigma = ', sigma)
+        min_n_variation.append([min_n, sigma])
+    write_multi_list(min_n_variation, './outputs/n_sigma_variation.txt')
+    return min_n_variation
+
+
+if __name__ == '__main__':
+    get_min_n_variation(MEAN_1, MEAN_2, N_experiments, ALPHA, BETA, SIGMA_RANGE)
